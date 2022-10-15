546ca590e22b0190443e15c584dd7af7aa6e3017
Lokisk1155 <shawn12345d@gmail.com>
2022-10-15 00:34:00 -0400

    progression on about tabbing between compoents | this is actually starting to personally offend me

--- frontend/src/components/ProfilePage/AboutPage/AboutPage.js
+++ frontend/src/components/ProfilePage/AboutPage/AboutPage.js
@@ -1,22 +1,68 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './AboutPage.css'
 import Overview from './Overview'
 import PlacesLived from './PlacesLived'
+import WorkEd from './WorkEd'
 
 function AboutPage({ renderString }) {
 
     const [overview, setOverview] = useState(true)
-    const [customOverView, setCustomOverview] = useState(false)
+    const [customOverview, setCustomOverview] = useState(false)
 
     const [placesLived, setPlacesLived] = useState(false)
     const [customPlacesLived, setCustomPlacesLived] = useState(false)
 
-    if (renderString === 'overview') {
-        overview = true 
-        placesLived = false 
-    } else if (renderString === 'placesLived') {
-        overview = false 
-        placesLived = true 
+    const [workEd, setWorkEd] = useState(false)
+    const [customWorkEd, setCustomWorkEd] = useState(false)
+
+
+    // if (renderString === 'overview') {
+    //     overview = true 
+    //     placesLived = false 
+    // } else if (renderString === 'placesLived') {
+    //     overview = false 
+    //     placesLived = true 
+    // }
+
+    // const preventDoubleRender = (state, func) => {
+    //     func(!state)
+    // }
+
+    // const handleRender = (action) => {
+    //     let value;
+    //     switch (action) {
+    //         case "overview":
+    //             value = <PlacesLived />
+    //             return
+    //         case "placesLived":
+    //             value = <Overview />
+    //             return
+    //     }
+    //     return value 
+    // }
+
+   
+
+   
+    function altOverview() {
+        setCustomPlacesLived(false)
+        setPlacesLived(false)
+        setCustomWorkEd(false)
+        setWorkEd(false)
+    }
+
+    function altWorkEd() {
+        setCustomPlacesLived(false)
+        setPlacesLived(false)
+        setCustomOverview(false)
+        setOverview(false)
+    }
+
+    function altPlacesLived() {
+        setCustomOverview(false)
+        setOverview(false)
+        setCustomWorkEd(false)
+        setWorkEd(false)
     }
 
 
@@ -24,16 +70,31 @@ function AboutPage({ renderString }) {
         <div className="about-page-container" >
             <div className='about-page-block'>
                 <div className='about-page-button-container'>
-                    <button tabindex="1" className='about-page-button-style'>Overview</button>
-                    <button tabindex="2" className='about-page-button-style'>Work and Education</button>
-                    <button tabindex="3" className='about-page-button-style'>Places Lived</button>
-                    <button tabindex="4" className='about-page-button-style'>Contact Info</button>
-                    <button tabindex="5" className='about-page-button-style'>Family and Relationships</button>
+                    <button tabIndex="1" className='about-page-button-style' onClick={(() => {
+                        setCustomOverview(!customOverview)
+                        setOverview(customOverview)
+                        altOverview()
+                    })}>Overview</button>
+                    <button tabIndex="2" className='about-page-button-style' onClick={(() => {
+                        setCustomWorkEd(!customWorkEd)
+                        setWorkEd(customWorkEd)
+                        altWorkEd()
+
+                    })}>Work and Education</button>
+                    <button tabIndex="3" className='about-page-button-style' onClick={(() => {
+                        setCustomPlacesLived(!customPlacesLived)
+                        setPlacesLived(customPlacesLived)
+                        altPlacesLived() 
+                    })}>Places Lived</button>
+                    <button tabIndex="4" className='about-page-button-style'>Contact Info</button>
+                    <button tabIndex="5" className='about-page-button-style'>Family and Relationships</button>
 
                 </div>
                     <div className='about-page-component-selector'>
                         {overview && <Overview />}
                         {placesLived && <PlacesLived />}
+                        {workEd && <WorkEd />}
+                        
   
                 </div>
             </div>
---
+++ frontend/src/components/ProfilePage/AboutPage/WorkEd.js
@@ -0,0 +1,8 @@
+function WorkEd() {
+
+    return (
+        <p>work and education</p>
+    )
+}
+
+export default WorkEd
