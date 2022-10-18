0c7ba30f674991210ff2d18ad1c9f8e87c863c76
Lokisk1155 <shawn12345d@gmail.com>
2022-10-18 10:42:24 -0400

    post routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :show, :update]
+    resources :iposts, only: [:create, :show, :index]
     resource :session, only: [:show, :create, :destroy]
+
   end
 
   get '*path', to: "static_pages#frontend_index"
