f3a143ba319794db78853a503b3e559a294434b0
Lokisk1155 <shawn12345d@gmail.com>
2022-10-19 17:59:40 -0400

    done with css for now, time to fix about page

--- frontend/src/components/HomePage/NavBar/ProfilePicModal.js
+++ frontend/src/components/HomePage/NavBar/ProfilePicModal.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom"
 import { logout } from "../../../store/session"
 import { useDispatch, useSelector } from "react-redux"
 import './profilePicModal.css'
+import profilePic from './imgs/blank.png'
 
 
 function ProfilePicModal({ closeModal }) {
@@ -11,12 +12,21 @@ function ProfilePicModal({ closeModal }) {
     return (
         <div className="omega-profile-modal-container">
             <div className="profile-pic-modal-container">
-                <div>
-                    <Link className="link-to-profile-page" to={`/ProfilePage/${user.id}`}>Profile Page!</Link>
-                </div>
+                
+                    <Link className="link-to-profile-page" to={`/ProfilePage/${user.id}`}>
+                        <button className="link-button">
+                            <img className="link-profile-pic" src={profilePic} />
+                            <p className="link-user-name">{user.first_name} {user.last_name}</p>
+                        </button>
+                    </Link>
+               
 
                 <div>
-                    <button className="logout-button" onClick={() => dispatch(logout())}>Sign Out</button>
+                <button className="logout-button" onClick={() => dispatch(logout())}>
+                    <label className="logout-button-text">
+                        Log Out
+                    </label>
+                </button>
                 </div>
             </div>
         </div>
