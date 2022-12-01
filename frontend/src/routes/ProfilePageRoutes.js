619aee837f33b039e3c854636f242275811b71d6
lokisk1155 <shawn12345d@gmail.com>
2022-12-01 10:57:41 -0500

    more route changes

--- frontend/src/routes/ProfilePageRoutes.js
+++ frontend/src/routes/ProfilePageRoutes.js
@@ -2,51 +2,65 @@ import { Switch, Route } from "react-router-dom"
 import ProfileHeader from "../components/ProfilePage/ProfileHeader"
 import AboutPage from "../components/ProfilePage/AboutPage/AboutPage"
 import ProfileTop from "../components/ProfilePage/ProfileTop"
+import Overview from "../components/ProfilePage/AboutPage/Overview"
 import HomePage from "../components/HomePage/HomePage"
 import Posts from "../components/ProfilePage/Posts"
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import WorkEd from "../components/ProfilePage/AboutPage/WorkEd"
+import PlacesLived from "../components/ProfilePage/AboutPage/PlacesLived"
+import ContactInfo from "../components/ProfilePage/AboutPage/ContactInfo"
+import Relationship from "../components/ProfilePage/AboutPage/Relationship"
+import Friends from "../components/ProfilePage/Friends"
 
 export function ProfilePageRoutes() {
+
+    useEffect(() => {
+
+    })
+
     return (
         <> 
             <Switch>
                 <Route exact path="/ProfilePage/:id">
-                    <ProfileHeader />
+                    <ProfileTop />
+                    <Posts />
                 </Route>
-
-                <Route exact path="/ProfilePage/:id/posts" /> 
 
                 <Route path="/ProfilePage/:id/about">
-
-                </Route>
-            
-                <Route exact path="/ProfilePage/:id/about/overview">
-
+                    <ProfileTop />
+                    <AboutPage  />
+                    <Overview />
                 </Route>
                         
                 <Route exact path="/ProfilePage/:id/about/work_and_education">
-
+                    <ProfileTop />
+                    <AboutPage  />
+                    <WorkEd />
                 </Route>
                         
                 <Route exact path="/ProfilePage/:id/about/places_lived">
-
+                    <ProfileTop />
+                    <AboutPage  />
+                    <PlacesLived />
                 </Route>
                         
                 <Route exact path="/ProfilePage/:id/about/Contact Info">
-
+                    <ProfileTop />
+                    <AboutPage  />
+                    <ContactInfo />
                 </Route>
                         
                 <Route exact path="/ProfilePage/:id/about/Family and Relationships">
-
+                    <ProfileTop />
+                    <AboutPage  />
+                    <Relationship />
                 </Route>
 
-                <Route exact path="/friends">
-
-                </Route>
-                    
-                <Route exact path="/photos">
-
-                </Route>
-                
+                <Route exact path="/ProfilePage/:id/friends">
+                    <ProfileTop /> 
+                    <Friends /> 
+                </Route>             
             </Switch>
         </>
     )
