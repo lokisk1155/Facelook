e244133bb5f85f9cb5e2d3597b9b6901a88a8b50
lokisk1155 <shawn12345d@gmail.com>
2022-12-08 17:48:32 -0500

    prettier

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -12,7 +12,7 @@ function NavBar() {
   const user = useSelector((state) => state.session.user);
   const [profileModal, setProfileModal] = useState(false);
   const [toggle, setToggle] = useState(true);
-  const [toggleSearch, setToggleSearch] = useState(false)
+  const [toggleSearch, setToggleSearch] = useState(false);
   const history = useHistory();
 
   function closeModal() {
@@ -26,46 +26,55 @@ function NavBar() {
   return (
     <div className="navbar-container">
       <div className="left-container-navbar">
-      <div className="facebook-picture">
-        <img
-          src={Facebook}
-          width="40px"
-          height="40px"
-          alt="facebook"
-          onClick={() => history.push("/")}
-        ></img>
+        <div className="facebook-picture">
+          <img
+            src={Facebook}
+            width="40px"
+            height="40px"
+            alt="facebook"
+            onClick={() => history.push("/")}
+          ></img>
+        </div>
+
+        {!toggleSearch && (
+          <div className="search-bar">
+            <input
+              type="text"
+              placeholder="    Search FaceLook"
+              className="search-input"
+              onClick={() => setToggleSearch(true)}
+            ></input>
+          </div>
+        )}
+
+        {toggleSearch && <SearchBar />}
       </div>
 
-
- {!toggleSearch && <div className="search-bar">
-        <input
-          type="text"
-          placeholder="    Search FaceLook"
-          className="search-input"
-          onClick={(() => setToggleSearch(true))}
-        ></input>
-    </div>}
-
-    {toggleSearch && <SearchBar />}
-
-    </div>
-
-    <div className="middle-column-navbar">
+      <div className="middle-column-navbar">
         <div className="home-link">
           <Link to="/">
             <i className="home-picture" />
           </Link>
         </div>
         <div className="github">
-          <a href="https://github.com/lokisk1155/FaceOok" target="_blank"><i className="github-pic"></i></a>
+          <a href="https://github.com/lokisk1155/FaceOok" target="_blank">
+            <i className="github-pic"></i>
+          </a>
         </div>
         <div className="linkedin">
-          <a href="https://www.linkedin.com/in/shawn-mallon-3050b7161" target="_blank"><i className="linkedin-picture"></i></a>
+          <a
+            href="https://www.linkedin.com/in/shawn-mallon-3050b7161"
+            target="_blank"
+          >
+            <i className="linkedin-picture"></i>
+          </a>
         </div>
         <div className="runaway">
-          <a href="https://lokisk1155.github.io/RunAway/" target="_blank"><i className="game-picture"></i></a>
+          <a href="https://lokisk1155.github.io/RunAway/" target="_blank">
+            <i className="game-picture"></i>
+          </a>
         </div>
-    </div>
+      </div>
 
       <div className="right-container-navbar">
         <div className="navbar-profile-modal">
@@ -78,19 +87,17 @@ function NavBar() {
               setProfileModal(toggle);
             }}
           />
-          </div>
-          </div>
-          {profileModal && (
-            <ProfilePicModal closeModal={closeModal} user={user} />
-          )}
+        </div>
       </div>
+      {profileModal && <ProfilePicModal closeModal={closeModal} user={user} />}
+    </div>
   );
 }
 
 export default NavBar;
 
-
-        {/* <img
+{
+  /* <img
           src={linkedin}
           alt="linkedin"
           className="linkedin"
@@ -103,4 +110,5 @@ export default NavBar;
           alt="github"
           className="github"
           onClick={() => window.open("https://github.com/lokisk1155/FaceOok")}
-        ></img> */}
+        ></img> */
+}
