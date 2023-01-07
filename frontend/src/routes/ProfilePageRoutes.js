413a6b53950f0bab1eaf97ccf008ff011523f3c9
Dan Mallon <d.mallon@me.com>
2023-01-07 14:39:00 -0500

    remove ProfileAbout component

--- frontend/src/routes/ProfilePageRoutes.js
+++ frontend/src/routes/ProfilePageRoutes.js
@@ -1,14 +1,17 @@
 import { Route } from "react-router-dom";
 import ProfileDefault from "../pages/profileDefault";
 import ProfileFriends from "../pages/profileFriends";
-import ProfileAbout from "../pages/profileAbout";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import ProfileTop from "../components/ProfilePage/ProfileTop";
 import { useEffect } from "react";
 import { profilePage } from "../store/profilePage";
-import AboutPage from "../components/ProfilePage/AboutPage/AboutPage";
 import Overview from "../components/ProfilePage/AboutPage/Overview";
+import AboutPage from "../components/ProfilePage/AboutPage/AboutPage";
+import ContactInfo from "../components/ProfilePage/AboutPage/ContactInfo";
+import PlacesLived from "../components/ProfilePage/AboutPage/PlacesLived";
+import Relationship from "../components/ProfilePage/AboutPage/relationship";
+import WorkEd from "../components/ProfilePage/AboutPage/WorkEd";
 
 export function ProfilePageRoutes() {
   const { id } = useParams();
@@ -37,27 +40,53 @@ export function ProfilePageRoutes() {
         path="/ProfilePage/:id/friends"
         render={() => <ProfileFriends />}
       />
-      <Route
-        exact
-        path="/ProfilePage/:id/about"
-        render={() => <ProfileAbout about={"Overview"} />}
-      />
-      <Route
-        path="/ProfilePage/:id/about/work_and_education"
-        render={() => <ProfileAbout about={"WorkEd"} />}
-      />
-      <Route
-        path="/ProfilePage/:id/about/places_lived"
-        render={() => <ProfileAbout about={"PlacesLived"} />}
-      />
-      <Route
-        path="/ProfilePage/:id/about/contact_info"
-        render={() => <ProfileAbout about={"Contact"} />}
-      />
-      <Route
-        path="/ProfilePage/:id/about/family_and_relationships"
-        render={() => <ProfileAbout about={"Relationship"} />}
-      />
+      <Route path="/ProfilePage/:id/about">
+        <div className="about-page-container">
+          <div className="about-page-block">
+            <AboutPage />
+            <Route
+              exact
+              path="/ProfilePage/:id/about"
+              render={() => (
+                <Overview sessionUser={sessionUser} currentUser={currentUser} />
+              )}
+            />
+            <Route
+              path="/ProfilePage/:id/about/work_and_education"
+              render={() => (
+                <WorkEd sessionUser={sessionUser} currentUser={currentUser} />
+              )}
+            />
+            <Route
+              path="/ProfilePage/:id/about/places_lived"
+              render={() => (
+                <PlacesLived
+                  sessionUser={sessionUser}
+                  currentUser={currentUser}
+                />
+              )}
+            />
+            <Route
+              path="/ProfilePage/:id/about/contact_info"
+              render={() => (
+                <ContactInfo
+                  sessionUser={sessionUser}
+                  currentUser={currentUser}
+                />
+              )}
+            />
+            <Route
+              path="/ProfilePage/:id/about/family_and_relationships"
+              render={() => (
+                <Relationship
+                  sessionUser={sessionUser}
+                  currentUser={currentUser}
+                />
+              )}
+            />
+          </div>
+        </div>
+      </Route>
     </>
   );
 }
