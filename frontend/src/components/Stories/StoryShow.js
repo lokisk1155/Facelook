3e3bb335839905241e175835f63e928f0d8c1fd1
Lokisk1155 <shawn12345d@gmail.com>
2023-01-18 10:52:53 -0500

    get data needed

--- frontend/src/components/Stories/StoryShow.js
+++ frontend/src/components/Stories/StoryShow.js
@@ -1,5 +1,32 @@
-function StoryShow() {
-  return <></>;
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getSimpleUsers } from "../../store/simpleUsers";
+import { useEffect } from "react";
+
+function StoryShow()  {
+    const dispatch = useDispatch() 
+    const { id } = useParams() 
+    const simpleUsers = useSelector((state) => state.simpleUsers)
+    const sessionUser = useSelector((state) => state.session.user)
+
+    useEffect(() => {
+        if (!simpleUsers || !sessionUser) {
+            getData() 
+        }
+        const getData = async() => {
+            await dispatch(getSimpleUsers())
+        }
+    }, [id])
+
+    if (!simpleUsers || !sessionUser) {
+        return null 
+    }
+  
+  return (
+    <>
+    <p>yo</p>
+    </>
+  )
 }
 
 export default StoryShow;
