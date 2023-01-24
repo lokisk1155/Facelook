bc5105d0f2b06ee263da92e1640558d6d3bdd8e8
Lokisk1155 <shawn12345d@gmail.com>
2023-01-24 14:32:22 -0500

    Navbar css changes

--- frontend/src/components/NavBar/ProfilePicModal.js
+++ frontend/src/components/NavBar/ProfilePicModal.js
@@ -3,48 +3,66 @@ import { logout } from "../../store/session";
 import { useDispatch, useSelector } from "react-redux";
 import "./profilePicModal.css";
 import profilePic from "./imgs/blank.png";
+import Github from "./imgs/GitHub.png"
+import Li from "./imgs/Li.png"
+import wellfound from "./imgs/wellfound.png"
 
 function ProfilePicModal({ closeModal }) {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.session.user);
+  const name = useSelector((state) => state.simpleUsers[user.id].name)
+
+  const developerPictures = [Github, Li, wellfound]
+  const developerWebsiteLinks = ["https://github.com/lokisk1155/FaceOok", "https://www.linkedin.com/in/shawn-mallon/", "https://angel.co/u/shawn-mallon"]
+  const developerWebsiteNames = ["Project's Repo", "Creator's Linkedin", "Creator's Wellfound"]
 
   return (
     <div className="omega-profile-modal-container">
-      <div className="profile-pic-modal-container">
-        <div
-          className="main-buttons-of-modal"
-          style={{
-            height: "35%",
-            backgroundColor: "#fff",
-            width: "90%",
-            marginleft: "5%",
-            marginRight: "5%",
-          }}
-        >
-          <Link to={`/ProfilePage/${user.id}`}></Link>
-          <div
-            style={{
-              height: "1px",
-              width: "90%",
-              marginLeft: "5%",
-              marginRight: "5%",
-              borderBottom: "1px solid lightgrey",
-            }}
-          ></div>
-          <Link to="/">
-            <button
-              className="logout-button"
-              onClick={() => {
-                dispatch(logout());
-              }}
-            >
-              <label className="logout-button-text">Log Out</label>
-            </button>
-          </Link>
-        </div>
+      <div className="profile-pic-modal-container" style={{ width: "350px", padding: "2.5px"}}>
+          <div className="container-header-profile-modal" style={{ height: "35%", width: "325px", backgroundColor: "#fff", borderRadius: "10px", margin: "10px", boxShadow: "0px 4px 4px 0px lightgrey", borderTop: "0.25px solid lightgrey"}}>
+                <Link  style={{ width: "100%", height: "35%",  textDecoration: "none"}} to={`/ProfilePage/${user.id}`}>
+                  <div className="link-too-profile-page-modal" style={{ height: "35%", width: "90%", marginTop: "7.5px", borderRadius: "7px", border: "none", display: "flex", alignItems: "center", padding: "5px", justifyContent: "flex-start", marginLeft: "2.5%", marginRight: "2.5%"}}>
+                  <img style={{ height: "90%", borderRadius: "50%"}} src={user.profile_picture || profilePic} />
+                  <h3 style={{   paddingLeft: "7px", textDecoration: "none", color: "black" }}>{name}</h3>
+                  </div>
+                </Link>
+                <div style={{ borderBottom: "1px solid lightgrey", width: "98%", margin: "1%"}}></div>
+                <button style={{ height: "35%", width: "95%", marginLeft: "2.5%", marginRight: "2.5%", marginTop: "1%", borderRadius: "7px", border: "none"}}  onClick={() => dispatch(logout(user))}>Logout</button>
+          </div>
+          <div className="person-links-container" style={{ height: "60%", width: "325px", marginRight: "10px", marginLeft: "10px", display: "flex", flexDirection: "column"}}>
+            {developerPictures.map((website, index) => {
+              return <Link className="developer-links" style={{ height: "28%", display: "flex", marginRight: developerPictures[index] === wellfound ? "5px" : "0px", margin: developerPictures[index] === wellfound ? "0px" : "2px", justifyContent: "flex-start", alignItems: "center", 
+              textDecoration: "none"}} to={developerWebsiteLinks[index]}>
+                      <img style={{height: "75%", padding: "1px"}} src={website} />
+                      <h3 style={{ textDecoration: "none", color: "black"}}>{developerWebsiteNames[index]}</h3>
+                    </Link>
+            })}
+          </div>
       </div>
-    </div>
+      </div>
+
   );
 }
 
 export default ProfilePicModal;
+{/* <div
+className="main-buttons-of-modal"
+style={{
+  height: "35%",
+  backgroundColor: "#fff",
+  width: "90%",
+  marginleft: "5%",
+  marginRight: "5%",
+}}
+>
+<Link to={`/ProfilePage/${user.id}`}></Link>
+<div
+  style={{
+    height: "1px",
+    width: "90%",
+    marginLeft: "5%",
+    marginRight: "5%",
+    borderBottom: "1px solid lightgrey",
+  }}
+></div>
+</div> */}
