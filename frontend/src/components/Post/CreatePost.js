20294f2c1308921081ca3cb6c51bcaf0bda18b5c
Lokisk1155 <shawn12345d@gmail.com>
2023-01-24 16:12:25 -0500

    prettier

--- frontend/src/components/Post/CreatePost.js
+++ frontend/src/components/Post/CreatePost.js
@@ -3,87 +3,109 @@ import { useDispatch } from "react-redux";
 import { createPost } from "../../store/post";
 import { useState } from "react";
 import { useEffect } from "react";
-import "./CreatePost.css"
+import "./CreatePost.css";
 import profilePic from "../NavBar/imgs/blank.png";
 import CropEasy from "../crop/CropEasy";
 
 function CreatePost({ closeModal }) {
-    const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-    const sessionUser = useSelector((state) => state.session.user)
+  const sessionUser = useSelector((state) => state.session.user);
 
-    const [content, setContent] = useState("");
+  const [content, setContent] = useState("");
 
-    const [photoFile, setPhotoFile] = useState(null);
+  const [photoFile, setPhotoFile] = useState(null);
 
-    const [photoUrl, setPhotoUrl] = useState(null);
-  
-    const [openCrop, setOpenCrop] = useState(false);
+  const [photoUrl, setPhotoUrl] = useState(null);
 
-    const [containerHeight, setContainerHeight] = useState(null);
+  const [openCrop, setOpenCrop] = useState(false);
 
-    useEffect(() => {
-        if (photoFile) {
-          setContainerHeight("600px");
-        } else {
-          setContainerHeight("375px");
-        }
-      }, [photoFile]);
-    
-    function handlePostSubmit() {
-        if (content.length < 1) return closeModal(null);
-        let formData;
-        if (photoFile) {
-          formData = new FormData();
-          formData.append("postAttached[photo]", photoFile);
-        }
-        const post = {
-            content, user_id: sessionUser.id
-        }
-        const id = null
-        const location = "home"
-        dispatch(createPost(post, id, location, formData));
-        return closeModal(null);
-      }
+  const [containerHeight, setContainerHeight] = useState(null);
 
-    const handleFile = (e) => {
-        const file = e.target.files[0];
-        if (file) {
-          const fileReader = new FileReader();
-          fileReader.readAsDataURL(file);
-          fileReader.onload = () => {
-            setPhotoFile(file);
-            setPhotoUrl(fileReader.result);
-          };
-        }
-        setOpenCrop(true);
+  useEffect(() => {
+    if (photoFile) {
+      setContainerHeight("600px");
+    } else {
+      setContainerHeight("375px");
+    }
+  }, [photoFile]);
+
+  function handlePostSubmit() {
+    if (content.length < 1) return closeModal(null);
+    let formData;
+    if (photoFile) {
+      formData = new FormData();
+      formData.append("postAttached[photo]", photoFile);
+    }
+    const post = {
+      content,
+      user_id: sessionUser.id,
+    };
+    const id = null;
+    const location = "home";
+    dispatch(createPost(post, id, location, formData));
+    return closeModal(null);
+  }
+
+  const handleFile = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const fileReader = new FileReader();
+      fileReader.readAsDataURL(file);
+      fileReader.onload = () => {
+        setPhotoFile(file);
+        setPhotoUrl(fileReader.result);
       };
+    }
+    setOpenCrop(true);
+  };
 
-    return (
-<div className="omega-create-post-modal" style={{ height: containerHeight }}>
-{openCrop ? (
-            <CropEasy
-              photoURL={photoUrl}
-              setOpenCrop={setOpenCrop}
-              setPhotoURL={setPhotoUrl}
-              setFile={setPhotoFile}
-            />
-          ) : <> <h3 style={{ textAlign: "center"}}>Create Post</h3>
-                <div style={{ width: "100%", borderBottom: "1px solid lightgrey"}}></div>
-                <div style={{ display: "flex", height: "15%", maxHeight: "45px", padding: "5px", alignItems: 'center'}}>
-                    <img style={{ height: "90%", borderRadius: "50%"}} src={sessionUser.profile_picture || profilePic}></img>
-                    <h3 style={{ marginLeft: "15px"}}>{`${sessionUser.first_name} ${sessionUser.last_name}`}</h3>
-                </div>
-                <textarea
+  return (
+    <div
+      className="omega-create-post-modal"
+      style={{ height: containerHeight }}
+    >
+      {openCrop ? (
+        <CropEasy
+          photoURL={photoUrl}
+          setOpenCrop={setOpenCrop}
+          setPhotoURL={setPhotoUrl}
+          setFile={setPhotoFile}
+        />
+      ) : (
+        <>
+          {" "}
+          <h3 style={{ textAlign: "center" }}>Create Post</h3>
+          <div
+            style={{ width: "100%", borderBottom: "1px solid lightgrey" }}
+          ></div>
+          <div
+            style={{
+              display: "flex",
+              height: "15%",
+              maxHeight: "45px",
+              padding: "5px",
+              alignItems: "center",
+            }}
+          >
+            <img
+              style={{ height: "90%", borderRadius: "50%" }}
+              src={sessionUser.profile_picture || profilePic}
+            ></img>
+            <h3
+              style={{ marginLeft: "15px" }}
+            >{`${sessionUser.first_name} ${sessionUser.last_name}`}</h3>
+          </div>
+          <textarea
             autoFocus={true}
             className="textarea-post"
             type="text"
             placeholder={`What's on your mind ${sessionUser.first_name}?`}
             onChange={(e) => setContent(e.target.value)}
           ></textarea>
-            {photoFile ? (
+          {photoFile ? (
             <img src={photoUrl} style={{ height: "50%", width: "100%" }} />
-            ) : (
+          ) : (
             <div style={{ width: "100%", height: "15%", alignItems: "center" }}>
               <label className="custom-file-upload">
                 <p>Add to your post</p>
@@ -108,16 +130,29 @@ function CreatePost({ closeModal }) {
                   onChange={handleFile}
                 />
               </label>
-            </div> )}
-            <div style={{ width: "100%", height: "12%", alignItems: "center" }}>
-               <button style={{ width: "98%", margin: "1%", height: "100%", maxHeight: "50px", border: "none", backgroundColor: content.length < 1 ? "lightgrey" : "#166fe5", color: content.length < 1? "black" : "white", borderRadius: "3px" }} onClick={handlePostSubmit}>
-                Post
-               </button>
             </div>
-            </> }
-        </div>
-    )
-
+          )}
+          <div style={{ width: "100%", height: "12%", alignItems: "center" }}>
+            <button
+              style={{
+                width: "98%",
+                margin: "1%",
+                height: "100%",
+                maxHeight: "50px",
+                border: "none",
+                backgroundColor: content.length < 1 ? "lightgrey" : "#166fe5",
+                color: content.length < 1 ? "black" : "white",
+                borderRadius: "3px",
+              }}
+              onClick={handlePostSubmit}
+            >
+              Post
+            </button>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
