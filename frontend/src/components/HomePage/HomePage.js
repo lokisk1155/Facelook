e6583069fe0bd70973edbe8c10c34f1ea31ae347
Lokisk1155 <shawn12345d@gmail.com>
2023-01-31 14:11:47 -0500

    loading animations homepage

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -6,6 +6,7 @@ import NavBar from "../NavBar/NavBar";
 import StoriesHomeFeed from "./Middle/StoriesHomeFeed";
 import PostFeed from "./Middle/PostFeed";
 import "./HomePage.css";
+import StoriesHeader from "./Middle/StoriesHeader";
 
 function HomePage() {
   const dispatch = useDispatch();
@@ -14,15 +15,13 @@ function HomePage() {
 
   const posts = useSelector((state) => state.posts);
 
-  let limit = 3;
+  const limit = 3;
+
+  const number = 6
 
   useEffect(() => {
     Promise.all([dispatch(fetchStories(limit)), dispatch(fetchPosts())]);
   }, []);
-
-  if (!stories || !posts) {
-    return null;
-  }
 
   return (
     <>
@@ -30,8 +29,23 @@ function HomePage() {
       <div className="home-page-container">
         <div className="column"></div>
         <div className="middle">
-          <StoriesHomeFeed stories={stories} />
-          <PostFeed />
+          {Object.keys(stories).length > 0 ? <StoriesHomeFeed stories={stories} /> : 
+          <>
+            <StoriesHeader />
+            <div className="skeleton" style={{  
+          display: "flex",
+          height: "175px",
+          minHeight: "75px",
+          justifyContent: "space-evenly",
+          backgroundColor: "#fff",
+          padding: "10px",
+          borderEndStartRadius: "10px",
+          borderEndEndRadius: "10px",
+          boxShadow: "0px 6px 6px 0px lightgrey",
+          marginBottom: "10px",}}/>
+          </>}
+          {Object.keys(posts).length > 0 ? <PostFeed /> : [...Array(number)].map(() => {
+          return <div className="skeleton" style={{ height: "300px", width: "100%", marginBottom: "15px", borderRadius: "5px"}} />})}
         </div>
 
         <div className="column"></div>
