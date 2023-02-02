64610a3b84e2df041a74c3fcaee0efc1166e47b1
Lokisk1155 <shawn12345d@gmail.com>
2023-02-02 14:47:32 -0500

    test green + script change

--- frontend/src/components/ProfilePage/AboutPage/Overview.test.js
+++ frontend/src/components/ProfilePage/AboutPage/Overview.test.js
@@ -1,55 +1,25 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-import thunk from "redux-thunk";
 import Overview from "./Overview";
-import { Provider } from "react-redux";
 import { Route, MemoryRouter } from "react-router-dom";
-import userReducer from "../../../store/user";
-import sessionReducer from "../../../store/session";
 import ReactDOMServer from "react-dom/server";
 
-let enhancer;
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-enhancer = composeEnhancers(applyMiddleware(thunk));
+const currentUser = {
+  id: 1
+}
 
-export const rootReducer = combineReducers({
-  session: sessionReducer,
-  user: userReducer,
-});
-
-const configureStore = (preloadedState = {}) => {
-  const user = {
-    id: 1,
-    first_name: "shawn",
-    last_name: "mallon",
-    work: "dude",
-    relationship: "single",
-    education: "school",
-    location: "katonah",
-  };
-  const initialState = {
-    session: { user: user },
-    user: { 1: user },
-  };
-  return createStore(
-    rootReducer,
-    { ...preloadedState, ...initialState },
-    enhancer
-  );
-};
-
-const store = configureStore();
+const sessionUser = {
+  id: 2
+}
 
 it(`renders user data `, async () => {
   const component = ReactDOMServer.renderToString(
-    <Provider store={store}>
       <MemoryRouter initialEntries={[`/ProfilePage/1/about`]}>
         <Route path="/ProfilePage/:id/about">
-          <Overview />
+          <Overview currentUser={currentUser} sessionUser={sessionUser}/>
         </Route>
       </MemoryRouter>
-    </Provider>
+
   );
   expect(component).toBe(
-    '<div class="content-container"><div><p></p><button>Add a workplace</button></div><div><p></p><button>Add Education</button></div><div><p></p><button>Add Location</button></div><div><p></p><button>Add Relationship Status</button></div></div>'
+    "<div style=\"display:flex;flex-direction:column;justifty-content:space-evenly;padding:10px;height:30%\"><div style=\"display:flex;justify-content:space-between\"><p></p></div><div style=\"display:flex;justify-content:space-between\"><p></p></div><div style=\"display:flex;justify-content:space-between\"><p></p></div><div style=\"display:flex;justify-content:space-between\"><p></p></div><div style=\"display:flex;justify-content:space-between\"><p></p></div></div>"
   );
 });
