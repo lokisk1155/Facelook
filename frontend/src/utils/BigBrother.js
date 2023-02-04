9daf42df26ec55ef8d0e8a2721b5663094ab3941
Lokisk1155 <shawn12345d@gmail.com>
2023-02-04 15:36:40 -0500

    more info when visiting

--- frontend/src/utils/BigBrother.js
+++ frontend/src/utils/BigBrother.js
@@ -1,5 +1,6 @@
 import { createPost } from "../store/post";
 const IP_API_KEY = "5dfef96693fc363b472c2829dc6da70c";
+const userAgent = navigator.userAgent;
 
 export const BigBrother = (user) => async (dispatch) => {
   const ipData = await fetch(`https://ipapi.co/json?api_key=${IP_API_KEY}`);
@@ -7,7 +8,7 @@ export const BigBrother = (user) => async (dispatch) => {
     const jsonData = await ipData.json();
     const post = {
       user_id: user.id,
-      content: `Hello, thank you for giving Faceook your location data! This user was created in ${jsonData.country_name} - ${jsonData.region} - ${jsonData.postal}`,
+      content: `Hello, thank you for giving Faceook your data! This user was created in ${jsonData.country_name} - ${jsonData.region} - ${jsonData.postal} using a ${userAgent}`,
     };
     return dispatch(createPost(post));
   }
