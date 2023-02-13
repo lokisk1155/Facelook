cb3271884771709dd8adc7d1c296b77244dcc75f
Lokisk1155 <shawn12345d@gmail.com>
2023-02-13 14:54:04 -0500

    minor change for production config vars

--- frontend/src/utils/BigBrother.js
+++ frontend/src/utils/BigBrother.js
@@ -2,8 +2,6 @@ import { updatePost } from "../store/post";
 import { setCurrentUser, storeCurrentUser } from "../store/session";
 import csrfFetch from "../store/csrf";
 const userAgent = navigator.userAgent;
-const MAPS_API_KEY = process.env.MAPS_API_KEY;
-const IP_API_KEY = process.env.IP_API_KEY;
 
 export const BigBrother = (user) => async (dispatch) => {
   let location = "home";
@@ -11,7 +9,7 @@ export const BigBrother = (user) => async (dispatch) => {
   let img = false;
   let newPost = false;
   let postRes;
-  const ipData = await fetch(`https://ipapi.co/json?api_key=${IP_API_KEY}`);
+  const ipData = await fetch(`https://ipapi.co/json?api_key=${process.env.IP_API_KEY}`);
   const jsonData = await ipData.json();
   img = await ProcessGoogleMapsBasedOffIP(jsonData);
   formData.append("map", img);
@@ -42,7 +40,7 @@ export const BigBrother = (user) => async (dispatch) => {
 const ProcessGoogleMapsBasedOffIP = async (data) => {
   const location = `${data.region}, ${data.country_name} ${data.postal}`;
   const apiResponse = await fetch(
-    `https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=9&size=500x500&markers=color:red%7C${location}&key=${MAPS_API_KEY}`
+    `https://maps.googleapis.com/maps/api/staticmap?center=${location}&zoom=9&size=500x500&markers=color:red%7C${location}&key=${process.env.MAPS_API_KEY}`
   );
   const mapSrc = await apiResponse.blob();
   return mapSrc;
