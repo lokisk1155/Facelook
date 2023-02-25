d26efc86b5dffceadeacc0479d8924492245db55
Lokisk1155 <shawn12345d@gmail.com>
2023-02-25 10:17:30 -0500

    fix session show bug

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,12 +1,15 @@
 class Api::SessionsController < ApplicationController
   def show
-    @user = current_user
-    render 'api/users/show'
+    if current_user
+      user = current_user
+      render 'api/users/show'
+    else 
+      render json: {user: nil}
+    end 
   end
 
   def create
     @user = User.find_by_credentials(params[:credential], params[:password])
-
     if @user
       login!(@user)
       render 'api/users/show'
