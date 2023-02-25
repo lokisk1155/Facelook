dee4025a314ed38c6952d0b118b898e40a92e227
Lokisk1155 <shawn12345d@gmail.com>
2023-02-25 11:31:22 -0500

    add profile pic to preview story

--- frontend/src/components/Stories/ProgressBar.js
+++ frontend/src/components/Stories/ProgressBar.js
@@ -1,18 +1,30 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import LoadingBar from "./LoadingBar";
+import profilePic from "../NavBar/imgs/blank.png";
 
 function ProgressBar({ stories, currentStoryId, currentWindow }) {
   const { id } = useParams();
 
   const [progressBarWidth, setProgressBarWidth] = useState(null);
 
+  const [profilePicture, setProfilePicture] = useState(null)
+
+  const simpleUsers = useSelector((state) => state.simpleUsers)
+
   useEffect(() => {
     const calc = 100 / Object.keys(stories).length;
     setProgressBarWidth(calc);
+    if (simpleUsers[id].profile_picture) {
+      setProfilePicture(simpleUsers[id].profile_picture)
+    } else {
+      setProfilePicture(profilePic)
+    }
   }, [id, stories]);
 
   return (
+    <>
     <div
       style={{
         position: "absolute",
@@ -50,7 +62,10 @@ function ProgressBar({ stories, currentStoryId, currentWindow }) {
         </>
       ))}
     </div>
+    <img style={{ position: "absolute", top: "5%", marginLeft: "20px", height: "50px", width: "50px", borderRadius: "50%"}} src={profilePicture}/>
+    </>
   );
 }
 
 export default ProgressBar;
+// simpleUsers[currentStoryUserId].picture || 
