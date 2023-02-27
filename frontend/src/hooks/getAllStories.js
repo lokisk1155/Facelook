2438e063a2bc64ec5094749ded0ceb6b4c9ea393
Lokisk1155 <shawn12345d@gmail.com>
2023-02-27 15:47:14 -0500

    simplify data fetching for stories

--- frontend/src/hooks/getAllStories.js
+++ frontend/src/hooks/getAllStories.js
@@ -3,29 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import StoryShow from "../components/Stories/StoryShow";
 import { fetchStories } from "../store/story";
-import { useState } from "react";
 import { getSimpleUsers } from "../store/simpleUsers";
 
 export default function GetAllStories() {
   const dispatch = useDispatch();
-
   const { id } = useParams();
 
   const simpleUsers = useSelector((state) => state.simpleUsers);
 
-  const [cashe, setCashe] = useState(null);
-
   useEffect(() => {
-    if (!cashe) {
-      dispatch(fetchStories()).then((data) => {
-        setCashe(data);
-      });
-    }
+    dispatch(fetchStories());
 
     if (!simpleUsers) {
       dispatch(getSimpleUsers());
     }
-  }, [id]);
+  }, []);
 
   return <StoryShow />;
 }
