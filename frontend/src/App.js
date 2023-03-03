d5191cef6fd618f95af8306e37bf9fd2abc94d78
Lokisk1155 <shawn12345d@gmail.com>
2023-03-03 09:14:56 -0500

    potential production fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useParams } from "react-router-dom";
 import LoginPage from "./components/UserLogin/LoginPage";
 import { ProfilePageRoutes } from "./routes/ProfilePageRoutes";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import HomePage from "./components/HomePage/HomePage";
 import CreateStoryIntro from "./components/Stories/CreateStory";
 import GetAllStories from "./hooks/getAllStories";
+import { useEffect } from "react";
+import { getSimpleUsers } from "./store/simpleUsers";
 
 function App() {
+  const dispatch = useDispatch();
+
   const sessionUser = useSelector((state) => state.session.user);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(getSimpleUsers());
+  }, [dispatch, id]);
   return (
     <>
       <Switch>
