499345eb5c9130dc71a0d3c017a1b59e0181211c
Lokisk1155 <shawn12345d@gmail.com>
2023-03-13 14:16:13 -0400

    move date functions to seperate file

--- frontend/src/components/Post/PostHeader.js
+++ frontend/src/components/Post/PostHeader.js
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import PostLoading from "../loading/PostLoading";
 import profilePic from "../NavBar/imgs/blank.png";
 import { userRemovePost } from "../../store/profilePage";
+import { getTimeElapsed } from "./utils/Date";
 function PostHeader({
   post,
   simpleUsers,
@@ -27,32 +28,7 @@ function PostHeader({
     });
   };
 
-  function getTimeElapsed(createdAt) {
-    const previous = new Date(createdAt);
-    const now = new Date();
-    const comparedTime = now.valueOf() - previous.valueOf();
-    return formatDateTime(comparedTime);
-  }
 
-  function formatDateTime(comparedTime) {
-    const sec = Math.floor(comparedTime / 1000);
-    if (sec < 60) {
-      return `${sec}s`;
-    }
-    const min = Math.floor(sec / 60);
-    if (min < 60) {
-      return `${min}m`;
-    }
-    const hr = Math.floor(min / 60);
-    if (hr < 24) {
-      return `${hr}h`;
-    }
-    const day = Math.floor(hr / 24);
-    if (day < 7) {
-      return `${day}d`;
-    }
-    return `${Math.floor(day / 7)}w`;
-  }
 
   return (
     <div
---
+++ frontend/src/components/Post/utils/Date.js
@@ -0,0 +1,26 @@
+export function getTimeElapsed(createdAt) {
+    const previous = new Date(createdAt);
+    const now = new Date();
+    const comparedTime = now.valueOf() - previous.valueOf();
+    return formatDateTime(comparedTime);
+  }
+
+  function formatDateTime(comparedTime) {
+    const sec = Math.floor(comparedTime / 1000);
+    if (sec < 60) {
+      return `${sec}s`;
+    }
+    const min = Math.floor(sec / 60);
+    if (min < 60) {
+      return `${min}m`;
+    }
+    const hr = Math.floor(min / 60);
+    if (hr < 24) {
+      return `${hr}h`;
+    }
+    const day = Math.floor(hr / 24);
+    if (day < 7) {
+      return `${day}d`;
+    }
+    return `${Math.floor(day / 7)}w`;
+  }
