08754c07f4decb6a47c51849b6a08b36a279c98b
lokisk1155 <shawn12345d@gmail.com>
2023-05-31 21:05:30 -0400

    colors for your shortcuts

--- frontend/src/components/HomePage/YourShortcuts.js
+++ frontend/src/components/HomePage/YourShortcuts.js
@@ -1,7 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faHeart, faDragon } from "@fortawesome/free-solid-svg-icons";
-import "./YourShortcuts.css";;
+import {
+  faEnvelope,
+  faHeart,
+  faDragon,
+} from "@fortawesome/free-solid-svg-icons";
+import "./YourShortcuts.css";
 
 function YourShortcuts() {
   const isViewportUnderCertainWidth = window.innerWidth < 768; // Adjust the width as per your needs
@@ -11,62 +15,62 @@ function YourShortcuts() {
   }
 
   return (
-      <article
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "80%",
-          width: "90%",
-          cursor: "pointer",
-          marginTop: "65px"
-        }}
+    <article
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "80%",
+        width: "90%",
+        cursor: "pointer",
+        marginTop: "65px",
+      }}
+    >
+      <a
+        className="icon-links"
+        href="https://www.linkedin.com/in/shawn-mallon/"
+        style={{ width: "90%", borderRadius: "5px", height: "75px" }}
       >
-        <a
-          className="icon-links"
-          href="https://www.linkedin.com/in/shawn-mallon/"
-          style={{ width: "90%", borderRadius: "5px", height: "75px" }}
-        >
-          {" "}
-          <FontAwesomeIcon size="xl" icon={faLinkedin} /> 
-          <p style={{ paddingLeft: "5px"}}>{"Linkedin"}</p>
-        </a>
-        <a
-          className="icon-links"
-          href="https://github.com/lokisk1155/FaceOok"
-          style={{ width: "90%", borderRadius: "5px", height: "75px" }}
-        >
-          {" "}
-          <FontAwesomeIcon size="xl" icon={faGithub} /> 
-          <p style={{ paddingLeft: "5px"}}>{"Github"}</p>
-        </a>
-        <a
-          className="icon-links"
-          href="https://mailmeaa.herokuapp.com/login"
-          style={{ width: "90%", borderRadius: "5px", height: "75px" }}
-        >
-          {" "}
-          <FontAwesomeIcon size="xl" icon={faEnvelope} /> 
-          <p style={{ paddingLeft: "5px"}}>{"Mail"}</p>
-        </a>
-        <a
-          className="icon-links"
-          href="https://bejewelled-cactus-d214e5.netlify.app/"
-          style={{ width: "90%", borderRadius: "5px", height: "75px" }}
-        >
-          {" "}
-          <FontAwesomeIcon size="xl" icon={faDragon} /> 
-          <p style={{ paddingLeft: "5px"}}>{"Threejs"}</p>
-        </a>
-        <a
-          className="icon-links"
-          href="https://github.com/rubyforgood/human-essentials"
-          style={{ width: "90%", borderRadius: "5px", height: "75px" }}
-        >
-          {" "}
-          <FontAwesomeIcon color="red" size="xl" icon={faHeart} /> 
-          <p style={{ paddingLeft: "5px"}}>{"Ruby For Good"}</p>
-        </a>
-      </article>
+        {" "}
+        <FontAwesomeIcon color="darkblue" size="xl" icon={faLinkedin} />
+        <p style={{ paddingLeft: "5px" }}>{"Linkedin"}</p>
+      </a>
+      <a
+        className="icon-links"
+        href="https://github.com/lokisk1155/FaceOok"
+        style={{ width: "90%", borderRadius: "5px", height: "75px" }}
+      >
+        {" "}
+        <FontAwesomeIcon color="black" size="xl" icon={faGithub} />
+        <p style={{ paddingLeft: "5px" }}>{"Github"}</p>
+      </a>
+      <a
+        className="icon-links"
+        href="https://mailmeaa.herokuapp.com/login"
+        style={{ width: "90%", borderRadius: "5px", height: "75px" }}
+      >
+        {" "}
+        <FontAwesomeIcon color="lightyellow" size="xl" icon={faEnvelope} />
+        <p style={{ paddingLeft: "5px" }}>{"Mail"}</p>
+      </a>
+      <a
+        className="icon-links"
+        href="https://bejewelled-cactus-d214e5.netlify.app/"
+        style={{ width: "90%", borderRadius: "5px", height: "75px" }}
+      >
+        {" "}
+        <FontAwesomeIcon color="green" size="xl" icon={faDragon} />
+        <p style={{ paddingLeft: "5px" }}>{"Threejs"}</p>
+      </a>
+      <a
+        className="icon-links"
+        href="https://github.com/rubyforgood/human-essentials"
+        style={{ width: "90%", borderRadius: "5px", height: "75px" }}
+      >
+        {" "}
+        <FontAwesomeIcon color="red" size="xl" icon={faHeart} />
+        <p style={{ paddingLeft: "5px" }}>{"Ruby For Good"}</p>
+      </a>
+    </article>
   );
 }
 
